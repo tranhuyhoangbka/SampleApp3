e6ac3779dfd8f033cf084e88533577556e435a3a
tranhuyhoangbka <hoangth92.nd@gmail.com>
2015-01-30 16:11:24 +0700

    Finish log in/log out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
+      log_in @user
   		flash[:success] = "account is created!"
   		redirect_to @user
   	else
