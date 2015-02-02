6a3880f60a35812865fbc904263897aeb5e7188e
tranhuyhoangbka <hoangth92.nd@gmail.com>
2015-02-02 09:53:56 +0700

    Finish user edit update index and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,9 @@ class SessionsController < ApplicationController
   def create
   	user = User.find_by(email: params[:session][:email].downcase)
   	if user && user.authenticate(params[:session][:password])
+      redirect_back_or user
   		log_in(user)
-  		params[:session][:remember_me] == 1 ? remember(user) : forget(user)
-  		redirect_to user
+  		params[:session][:remember_me] == 1 ? remember(user) : forget(user)  		
   	else
   		flash.now[:danger] = "Invalid email/password combination"
   		render 'new'
